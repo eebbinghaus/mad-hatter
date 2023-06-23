2f19f9bc336a2996716adc9f2890f4b224713dba
Erich <eebbinghaus@gmail.com>
2023-06-22 19:09:31 -0600

    created rough navbar as well as set up multiple pages for the site

--- react-mad-hatter/src/App.jsx
+++ react-mad-hatter/src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Account from "./pages/Account";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Sale from "./pages/Sale";
+
 function App() {
-    return <div className="w-screen h-screen bg-red-300"></div>;
+    return (
+        <div className="w-screen h-screen bg-red-300">
+            <Router>
+                <Navbar />
+
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/account" element={<Account />} />
+                    <Route exact path="/cart" element={<Cart />} />
+                    <Route exact path="/contact" element={<Contact />} />
+                    <Route exact path="/sale" element={<Sale />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ react-mad-hatter/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul className="flex justify-end">
+                <li>
+                    <Link className={"mr-8"} aria-current="page" to="/sale">
+                        Sale
+                    </Link>
+                </li>
+                <li>
+                    <Link className={"mr-8"} aria-current="page" to="/contact">
+                        Contact Us
+                    </Link>
+                </li>
+                <li>
+                    <Link className={"mr-8"} aria-current="page" to="/account">
+                        My Account
+                    </Link>
+                </li>
+                <li>
+                    <Link className={"mr-8"} aria-current="page" to="/cart">
+                        Cart
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Navbar;
